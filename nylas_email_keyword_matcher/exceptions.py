8b6fc5d9668392575b10a8171d66c8c226304f32
Audrow Nash <audrow.nash@gmail.com>
2020-10-19 21:03:26 -0700

    Make a common import for exceptions
    Signed-off-by: Audrow Nash <audrow.nash@gmail.com>

---
+++ nylas_email_keyword_matcher/exceptions.py
@@ -0,0 +1,4 @@
+from nylas_email_keyword_matcher.keyword_matcher import NoKeywordFound
+from nylas_email_keyword_matcher.nylas_emailer import \
+    NoEmailReplyError, NoThreadFoundError, MultipleMatchingEmailThreadsError
+from nylas_email_keyword_matcher.nylas_context_manager import NylasError
